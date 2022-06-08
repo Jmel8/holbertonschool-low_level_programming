4bab9c3f5b1014306305837f1faee11134eae1d7
Jmel8 <5152@holbertonschool.com>
2022-06-08 23:27:55 +0000

    Create program that prints lowercase alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+ /**
+  * main - Entry point
+  *
+  * Return: Always 0 (success)
+  */
+int main(void)
+{
+	int i;
+
+	i = 97;
+	while (i <= 122)
+	{
+	putchar(i);
+	i++;
+	}
+
+	putchar(10);
+	return (0);	
+}
