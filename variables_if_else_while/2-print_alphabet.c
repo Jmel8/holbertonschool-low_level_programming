a9f57c2998f4a8f889c50199b1b0a2fc5634602d
Jmel8 <5152@holbertonschool.com>
2022-06-08 23:35:18 +0000

    Fix betty style

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
- /**
-  * main - Entry point
-  *
-  * Return: Always 0 (success)
-  */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
 int main(void)
 {
 	int i;
@@ -16,5 +17,5 @@ int main(void)
 	}
 
 	putchar(10);
-	return (0);	
+	return (0);
 }
