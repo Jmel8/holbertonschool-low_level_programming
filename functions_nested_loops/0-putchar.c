3b0fd7725a59f236f15cc8feca31ec6da3a95a00
Jmel8 <5152@holbertonschool.com>
2022-06-09 21:45:01 +0000

    Write a program that prints _putchar, followed by a new line

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,28 @@
+#include "funcionesj.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+int q;
+int w;
+int e;
+int r;
+int t;
+int y;
+int u;
+int o;
+_putchar(i = 95);
+_putchar(q = 112);
+_putchar(w = 117);
+_putchar(e = 116);
+_putchar(r = 99);
+_putchar(t = 104);
+_putchar(y = 97);
+_putchar(u = 114);
+_putchar(o = 10);
+return (0);
+}
---
+++ functions_nested_loops/_putchar.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "funcionesj.h"
+
+int _putchar(int i)
+{	
+	putchar(i);
+	return (0);
+}
