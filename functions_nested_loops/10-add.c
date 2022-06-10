957c25efc066983efa20c3aec545c23481f8be14
Jmel8 <5152@holbertonschool.com>
2022-06-10 01:25:36 +0000

    Modify the file 10-add.c

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,12 +1,17 @@
 #include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-	int i;
-	int o;
-	int n;
-	i = 89;
-	o = 9;
-	n = suma(i, o);
-	printf("%d\n", n);
-	return (0);
+int i;
+int o;
+int n;
+i = 89;
+o = 9;
+n = suma(i, o);
+printf("%d\n", n);
+return (0);
 }
