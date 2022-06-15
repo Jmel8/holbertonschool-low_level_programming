5bdb2bf36c90afd644fe048a478dc7cfeff27a4f
Jmel8 <jmelgarejomauricio@gmail.com>
2022-06-15 11:02:41 -0700

    Write a program that prints the numbers from 1 to 100

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -6,16 +6,20 @@
 
 int main(void)
 {
+	int i;
+
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3 == 0 && i % 5 == 0))
-			printf("%s ", "FizzBuzz");
-		else if ((i % 3) == 0)
-			printf("%s ", "Fizz");
-		else if ((i % 5) == 0)
-			printf("%s ", "Buzz");
+		if (i % 15 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
 		else
-			printf("%d ", i);
+			printf("%d", i);
+		if (i < 100)
+			printf(" ");
 	}
 	putchar('\n');
 	return (0);
