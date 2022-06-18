47f9291ad3334935d6b6a70fd541b8b4ae9ed82c
Jmel8 <jmelgarejomauricio@gmail.com>
2022-06-17 17:57:59 -0700

    Modify the code

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -10,10 +10,8 @@ void puts2(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] % 2 == 0)
-		{
-			_putchar(str[i]);
-		}
+		if (i % 2 == 0)
+		_putchar(str[i]);
 	}
 	_putchar(10);
 }
