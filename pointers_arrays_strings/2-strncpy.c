39c7283c4a4080ea5d67dab9af285853b64f9d7d
Jmel8 <jmelgarejomauricio@gmail.com>
2022-06-21 15:29:05 -0700

    Write a function that copies a string

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 /**
  * *_strncpy - Write a function that copies a string
  * @dest: string to concatenate
@@ -9,6 +8,16 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	strncpy(dest, src, n);
+	int i;
+
+	for (i = 0; i < n && *(src + i) != '\0'; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	while (i < n)
+	{
+		*(dest + i) = '\0';
+		i++;
+	}
 	return (dest);
 }
