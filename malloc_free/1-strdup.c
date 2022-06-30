4740c41fe2e124d01e91ab8a79c95baa24070e9b
Jmel8 <jmelgarejomauricio@gmail.com>
2022-06-30 08:59:08 -0700

    Write a function that returns a pointer to a newly allocated space

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strdup -  returns a pointer to a newly allocated space in memory 
+ * @str: string to be duplicated
+ * Return: a string pointer
+ */
+char *_strdup(char *str)
+{
+	int i = 1, k = 0;
+	char *j;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[i])
+		i++;
+	j = malloc(i * sizeof(char) + 1);
+	if (j == NULL)
+		return (NULL);
+	while (k < i)
+	{
+		j[k] = str[k];
+		k++;
+	}
+	j[k] = '\0';
+	return (j);
+}	
