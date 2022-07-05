c8f9fe08c06bac7b6dfe0c2be6fee13ff1b1facb
Jmel8 <jmelgarejomauricio@gmail.com>
2022-07-05 16:58:51 -0700

    Create a new information structure

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+/**
+ * struct dog - Data of my pet
+ * @name: First parameter
+ * @age: Second Parameter
+ * @owner: Third parameter
+ * Description: dog information
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
